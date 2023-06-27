cd51a12e067e0f68215640197733061a00e78128
angelospk <rovas450@gmail.com>
2023-06-28 00:48:39 +0300

    arrows don't work key arrow pressed work

--- src/routes/ekthesi/[slug]/+page.svelte
+++ src/routes/ekthesi/[slug]/+page.svelte
@@ -1,31 +1,75 @@
 <script lang="ts">
+    import { onMount, onDestroy } from 'svelte';
+    
     export let data;
     
     let isModalOpen = false;
     let currentImage = '';
+    let currentIndex = 0;
 
     const images = data.data;
     
-    function handleClick(imgUrl) {
+
+    let modalClass = "modal";
+    
+    function handleClick(imgUrl, index) {
         currentImage = imgUrl;
+        currentIndex = index;
         isModalOpen = true;
+        modalClass = "modal open"; // Add class to trigger transition
+        prefetchAdjacentImages();
     }
 
     function closeModal() {
         isModalOpen = false;
+        modalClass = "modal"; // Reset class
     }
     
-    function handleMouseMove(event) {
-        const {left, top, width, height} = event.currentTarget.getBoundingClientRect();
-        const x = ((event.clientX - left) / width) * 100;
-        const y = ((event.clientY - top) / height) * 100;
-        event.currentTarget.style.backgroundPosition = `${x}% ${y}%`;
+    function goToNextImage() {
+        if (currentIndex < images.length - 1) {
+            currentIndex++;
+            currentImage = images[currentIndex].src;
+            prefetchAdjacentImages();
+        }
+    }
+
+    function goToPreviousImage() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            currentImage = images[currentIndex].src;
+            prefetchAdjacentImages();
+        }
+    }
+
+    function handleKeyDown(event) {
+        if (event.key === 'ArrowRight') {
+            goToNextImage();
+        } else if (event.key === 'ArrowLeft') {
+            goToPreviousImage();
+        }
     }
 
     function prefetchImage(imgUrl) {
         const img = new Image();
         img.src = imgUrl;
     }
+
+    function prefetchAdjacentImages() {
+        if (currentIndex > 0) {
+            prefetchImage(images[currentIndex - 1].src);
+        }
+        if (currentIndex < images.length - 1) {
+            prefetchImage(images[currentIndex + 1].src);
+        }
+    }
+ 
+    onMount(() => {
+        window.addEventListener('keydown', handleKeyDown);
+    });
+
+    onDestroy(() => {
+        window.removeEventListener('keydown', handleKeyDown);
+    });
 </script>
 
 <style>
@@ -39,23 +83,44 @@
         display: flex;
         justify-content: center;
         align-items: center;
+        z-index: 10;
     }
 
     .magnified-image {
-        width: 90%;
-        height: 90%;
+        position: relative;
+        width: 85%;
+        height: 85%;
         background-repeat: no-repeat;
         background-size: 100%;
+    }
+
+    .arrow {
+        position: absolute;
+        top: 50%;
+        font-size: 5em;
+        color: white;
+        cursor: pointer;
+        user-select: none;
+    }
+
+    .left-arrow {
+        left: 10px;
+    }
+
+    .right-arrow {
+        right: 10px;
     }
 </style>
 
 {#if isModalOpen}
-    <div class="modal" on:click={closeModal}>
+    <div class={modalClass} on:mouseout={closeModal}>
         <div 
             class="magnified-image"
             style="background-image: url({currentImage})"
-            on:mousemove={handleMouseMove}
-        ></div>
+        >
+            <span class="arrow left-arrow" on:click={goToPreviousImage}>&lt;</span>
+            <span class="arrow right-arrow" on:click={goToNextImage}>&gt;</span>
+        </div>
     </div>
 {/if}
 
@@ -69,9 +134,9 @@
 
 {#if images && images.length > 0}
     <div class="flex flex-wrap overflow-hidden">
-    {#each images as item}
+    {#each images as item, index}
         <img 
-            on:click={() => handleClick(item.src)} 
+            on:click={() => handleClick(item.src, index)} 
             on:mouseenter={() => prefetchImage(item.src)}
             class="m-3 mx-auto hover:cursor-zoom-in" 
             src={item.src.replace("cdn","media").replace("com","net").replace("jpg","jpg?width=240&height=160")} 
